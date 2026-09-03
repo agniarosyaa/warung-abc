600e9b8be2fbb0674fbc3e1ad0cd3e64b5cf17bc
agnia rosya noerillah <agniarosyaa@gmail.com>
2026-09-03 11:21:19 +0700

    Update riwayat_transaksi.php

--- riwayat_transaksi.php
+++ riwayat_transaksi.php
@@ -10,28 +10,29 @@ $sql .= " ORDER BY t.tanggal DESC";
 
 $hasil = mysqli_query($koneksi, $sql);
 if(!$hasil) {
-    die("Error SQL:" . mysqli_error ($koneksi));
+    die("Error SQL:" . mysqli_error($koneksi));
 }
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Riwayat Transaksi - Warung ABC</title>
-    <link rel="stylesheet" href="css/style.css">
-    </head>
-    <body>
-        <h1>Riwayat Transaksi</h1>
-        <table border="1" cellpadding="6">
-            <tr><th>No. Transaksi</th><th>Tanggal</th><th>Kasir</th><th>Total Bayar</th><th>Aksi</th></tr>
-                <?php while ($row = mysqli_fetch_assoc($hasil)) { ?>
-                <tr>
-                    <td><?php echo $row['no_transaksi']; ?></td>
-                    <td><?php echo $row['tanggal']; ?></td>
-                    <td><?php echo $row['nama_kasir']; ?></td>
-                    <td><?php echo number_format($row['total_bayar'],0,',', '.'); ?></td>
-                    <td><a href="struk.php?id=<?php echo $row['id_transaksi']; ?>">Cetak</a></td>
-            </tr>
-            <?php } ?>
+<head>
+    <title>Riwayat Transaksi - Warung ABC</title>
+ <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>Riwayat Transaksi</h1>
+    <table border="1" cellpadding="6">
+        <tr><th>No.Transaksi</th><th>Tanggal</th><th>Kasir</th>
+        <th>Total Bayar</th><th>Aksi</th></tr>
+        <?php while ($row = mysqli_fetch_assoc($hasil)) { ?>
+        <tr> 
+            <td><?php echo $row['no_transaksi']; ?></td>
+            <td><?php echo $row['tanggal']; ?></td>
+            <td><?php echo $row['nama_kasir']; ?></td>
+            <td><?php echo number_format($row['total_bayar'], 0, ',', '.'); ?></td>
+            <td><a href="struk.php?id=<?php echo $row['id_transaksi']; ?>">Cetak</a></td>
+        </tr>
+        <?php } ?> 
     </table>
     <p><a href="dashboard.php">Kembali ke Dashboard</a></p>
 </body>
